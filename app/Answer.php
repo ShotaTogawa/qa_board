27e2e1a05b155b3bc95f015a35bf1fd466e685c4
Shota Togawa <s.togawa0112@gmail.com>
2019-03-02 23:40:18 -0800

    Update best_answer_id on questions table when answer removed in database layer

--- app/Answer.php
+++ app/Answer.php
@@ -28,12 +28,7 @@ class Answer extends Model
         }); 
 
         static::deleted(function ($answer) {
-            $question = $answer->question;
-            $question->decrement('answers_count');
-            if ($question->best_answer_id === $answer->id) {
-                $question->best_answer_id = Null;
-                $question->save();
-            }
+            $answer->question->decrement('answers_count');
         });
     }
 
